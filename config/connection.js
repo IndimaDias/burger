eecd47442443cea09d0a1f8f83d29dc7e40dadae
IndimaDias <indimad@yahoo.com>
2019-06-26 22:25:28 -0400

    create database and databse connection file

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// import npm mysql package 
+var mysql = require ("mysql");
+
+// create a connection to the database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "unhdb1",
+    database: "burgers_db"
+});
+
+// connect to the database
+connection.connect(function(err){
+    if(err){
+        console.log("Error connecting " + err.stack);
+        return;
+    }
+    console.log("Connected as id " + connection.threadId);
+});
+
+// export the connection to be used by the orm
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chicken burger",FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Beef and Cheese burger",FALSE);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheese burger",FALSE); 
