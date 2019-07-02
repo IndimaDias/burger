323c995c7c69e0d2ec8f2aa5c7be693755d3f5df
IndimaDias <indimad@yahoo.com>
2019-07-01 22:52:43 -0400

    Setup controller files. add router

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+var express = require("express");
+
+// import bugers in models 
+var burger = require("../models/burger.js");
+
+// create a router
+var router = express.router();
+
+// create routers for the app
+
+router.get("/",function(req,res){
+    burger.selectAll(function(data){
+        var burgerObj = {burgers : data};
+        res.render("index",burgerObj);
+    });
+});
+
+router.post("/api/burgers",function(req,res){
+    burger.insertOne(
+        ["burger_name","devoured"],
+        [req.body.name,req.body.devoured],
+        function(result){
+            res.json({id : result.id});
+        });
+});
+
+
+router.put("/api/cats/:id",function(req,res){
+    var condition = "id = " + req.params.id;
+
+    burger.updateOne({devoured:req.body.devoured }, condition,function(result){
+        if(result.changedRows == 0 ){
+            return res.status(404).end()
+
+            }
+            else{
+                res.status(200).end();
+            }
+        });
+    });
+
+module.exports = router;
